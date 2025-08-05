d92208c8277793b132979ccca79c54e9110ded14
erika fernanda pesca alfonso <erika_pesca@soy.sena.edu.co>
2025-08-04 19:06:29 -0500

    archivo de repositorio remoto

---
+++ repositorio_remoto.py
@@ -0,0 +1,7 @@
+"""
+https://github.com/Erika-pESca/modulo1.git
+Repositorio remoto para el curso de Python de Erika P. Escaño.
+Este repositorio contiene ejercicios prácticos de Python, cada uno con su propia funcionalidad y propósito
+
+aprendiz: Erika Fernanda Pesca Alfonso de la ficha 2993648 ADSO
+"""
